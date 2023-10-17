aab28469c55a22627dd8b4d6eecf6be1a2abc405
Manikya-Sharma <92919520+Manikya-Sharma@users.noreply.github.com>
2023-10-17 19:01:29 +0530

    changed emails to names and added redirect to chat

--- app/components/Posts/Replies.tsx
+++ app/components/Posts/Replies.tsx
@@ -2,6 +2,7 @@
 import { Reply } from "@/types/post";
 import { useState, useEffect } from "react";
 import ShowMarkdown from "../Markdown/ShowMarkdown";
+import { State } from "@/types/user";
 
 type Props = {
   replyIds: string[];
@@ -9,16 +10,46 @@ type Props = {
 
 export default function Replies(props: Props) {
   const [replies, setReplies] = useState<Array<Reply | null> | null>(null);
+  const [authors, setAuthors] = useState<Array<{
+    email: string;
+    data: State;
+  }> | null>(null);
   useEffect(() => {
     async function fetchData() {
+      // get replies
       const promises = props.replyIds.map((replyId) => {
         return fetch(`/api/posts/getReplies/${replyId}`);
       });
       const responses = await Promise.all(promises);
-      const data = (await Promise.all(
+      let data = (await Promise.all(
         responses.map((response) => response.json()),
       )) as Array<Reply | null> | null;
+      if (data == null) {
+        return;
+      }
+      data = data.filter((elem) => elem != null);
       setReplies(data);
+
+      // get authors
+      const authorEmails = data.map((reply) => {
+        if (reply == null) {
+          return "";
+        }
+        return reply.author;
+      });
+      const authorPromises = authorEmails.map((author) => {
+        return fetch(`/api/getAccountByEmail/${author}`);
+      });
+      const authorResponses = await Promise.all(authorPromises);
+      const authors = (await Promise.all(
+        authorResponses.map((author) => author.json()),
+      )) as Array<State>;
+
+      const authorsArray: Array<{ email: string; data: State }> = [];
+      for (let i = 0; i < authorEmails.length; i++) {
+        authorsArray.push({ email: authorEmails[i], data: authors[i] });
+      }
+      setAuthors(authorsArray);
     }
     fetchData();
   }, [props.replyIds]);
@@ -30,7 +61,11 @@ export default function Replies(props: Props) {
       >
         {reply && <ShowMarkdown data={reply.content} />}
         <cite className="mr-auto block w-fit text-sm text-slate-400">
-          - {reply?.author}
+          -{" "}
+          {authors &&
+            authors
+              .filter((author) => author.email == reply?.author)[0]
+              .data.name.split(" ")[0]}
         </cite>
       </div>
     );
